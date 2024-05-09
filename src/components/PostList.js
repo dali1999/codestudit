f23d19b86def3b4e52e1355d6d2a816b54c1f1b6
dali1999 <dali1999@naver.com>
2024-05-09 16:03:40 +0900

    feat: query status를 사용해서 로딩, 에러 처리

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,6 +3,8 @@ import { getPosts, getPostsByUsername } from "../api";
 import Post from "./Post";
 import { FEED_VARIANT } from "../values";
 import styles from "./PostList.module.css";
+import LoadingPage from "../pages/LoadingPage";
+import ErrorPage from "../pages/ErrorPage";
 
 function PostList({ variant = FEED_VARIANT.HOME_FEED }) {
   let postsQueryKey;
@@ -19,15 +21,25 @@ function PostList({ variant = FEED_VARIANT.HOME_FEED }) {
     console.warn("Invalid feed request.");
   }
 
-  const { data: postsData } = useQuery({
+  const {
+    data: postsData,
+    isPending,
+    isError,
+  } = useQuery({
     queryKey: postsQueryKey,
     queryFn: postsQueryFn,
+    retry: 0,
   });
+
+  if (isPending) return <LoadingPage />;
+  if (isError) return <ErrorPage />;
 
   const posts = postsData?.results ?? [];
 
   return (
     <div className={styles.postList}>
+      {isPending && <div>Loading</div>}
+      {isError && <div>ERROR!</div>}
       {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
