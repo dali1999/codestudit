4f20661d4ed31298704e448b450f5438c61b40e0
Jules <dali1999@naver.com>
2024-05-12 04:57:38 +0900

    feat: query의 enabled를 사용해 간단한 로그인 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,19 @@ import { Outlet } from "react-router-dom";
 import styles from "./App.module.css";
 import "./App.font.css";
 import Navigation from "./Navigation";
+import { useState } from "react";
+import { LoginContext } from "../context/LoginContext";
 
 function App() {
+  const [currentUsername, setCurrentUsername] = useState();
+
   return (
-    <>
+    <LoginContext.Provider value={{ currentUsername, setCurrentUsername }}>
       <Navigation className={styles.nav} />
       <div className={styles.body}>
         <Outlet />
       </div>
-    </>
+    </LoginContext.Provider>
   );
 }
 
