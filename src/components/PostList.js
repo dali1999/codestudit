7141f8d00c67c00ccd0ece02b0b88c7a5aa7cecd
Jules <dali1999@naver.com>
2024-05-12 04:58:34 +0900

    feat: query를 사용해 포스트 목록을 페이지네이션으로 구현

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,9 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  keepPreviousData,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { getPosts, getPostsByUsername, uploadPost } from "../api";
 import Post from "./Post";
 import { FEED_VARIANT } from "../values";
@@ -7,16 +12,20 @@ import LoadingPage from "../pages/LoadingPage";
 import ErrorPage from "../pages/ErrorPage";
 import PostForm from "./PostForm";
 import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
+
+const PAGE_LIMIT = 5;
 
 function PostList({ variant = FEED_VARIANT.HOME_FEED, showPostForm }) {
+  const [page, setPage] = useState(0);
   const queryClient = useQueryClient();
 
   let postsQueryKey;
   let postsQueryFn;
 
   if (variant === FEED_VARIANT.HOME_FEED) {
-    postsQueryKey = ["posts"];
-    postsQueryFn = getPosts;
+    postsQueryKey = ["posts", page];
+    postsQueryFn = () => getPosts(page, PAGE_LIMIT);
   } else if (variant === FEED_VARIANT.MY_FEED) {
     const username = "codeit";
     postsQueryKey = ["posts", username];
@@ -29,9 +38,14 @@ function PostList({ variant = FEED_VARIANT.HOME_FEED, showPostForm }) {
     data: postsData,
     isPending,
     isError,
+    isPlaceholderData,
   } = useQuery({
     queryKey: postsQueryKey,
     queryFn: postsQueryFn,
+
+    //매번 pending 상태가 되지 않고, 이전 데이터를 유지해서 보여주다가
+    //새로운 데이터 fetch가 완료 시 새로운 데이터로 바꿔서 보여줌
+    placeholderData: keepPreviousData,
   });
 
   const uploadPostMutation = useMutation({
@@ -54,6 +68,15 @@ function PostList({ variant = FEED_VARIANT.HOME_FEED, showPostForm }) {
 
   const posts = postsData?.results ?? [];
 
+  // useEffect(() => {
+  //   if (!isPlaceholderData && postsData?.hasMore) {
+  //     queryClient.prefetchQuery({
+  //       queryKey: ["posts", page + 1],
+  //       queryFn: () => getPosts(page + 1, PAGE_LIMIT),
+  //     });
+  //   }
+  // }, [isPlaceholderData, postsData, queryClient, page]);
+
   return (
     <div className={styles.postList}>
       {showPostForm ? (
@@ -67,6 +90,20 @@ function PostList({ variant = FEED_VARIANT.HOME_FEED, showPostForm }) {
       {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
+      <div>
+        <button
+          disabled={page === 0}
+          onClick={() => setPage((old) => Math.max(old - 1, 0))}
+        >
+          &lt;
+        </button>
+        <button
+          disabled={isPlaceholderData || !postsData?.hasMore}
+          onClick={() => setPage((old) => old + 1)}
+        >
+          &gt;
+        </button>
+      </div>
     </div>
   );
 }
